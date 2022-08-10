0c7461478dea1c8ba5d7e712c5f3e861d2460ed6
Dave Roppo <DavidRoppo@protonmail.com>
2022-08-10 16:49:53 -0500

    Update TCPclient.py

--- Python/TCP Client & Server/TCPclient.py
+++ Python/TCP Client & Server/TCPclient.py
@@ -1,1 +1,19 @@
+#import socket library
+import socket
+#Create socket object
+clientsocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+host = socket.gethostname()
+
+port = 444
+
+clientsocket.connect((host, port))
+#Set max amount of data 
+message = clientsocket.recv(1024)
+
+#Close the connection
+clientsocket.close()
+#decode ascii
+print(message.decode('ascii'))
+
 
